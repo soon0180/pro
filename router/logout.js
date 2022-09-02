00131be42806cf110437cf1c30ccb67a3565f2bb
soon0180 <https://github.com/soon0180>
2022-09-02 11:18:17 +0900

    ss

--- router/logout.js
+++ router/logout.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/logout", (req, res) => {
-  if (req.session?.access_token) {
+  if (req.session.access_token) {
     req.session.access_token = null;
     req.session.refresh_token = null;
     res.redirect("/");
